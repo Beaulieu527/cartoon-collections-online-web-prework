725ba4933642f3d7b3cdf6dd5ef5019cd8381b79
beaulieudev <beaulieudev@gmail.com>
2018-10-19 01:05:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,10 @@ def long_planeteer_calls(long_calls)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheesey)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheesy_items.find do |cheese|
+    cheesey.include?(cheese)
+  end
 end
